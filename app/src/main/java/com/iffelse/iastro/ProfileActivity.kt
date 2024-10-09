be95d5d03bb7f36cbc277aa738d6deae5578661c
Shubham Agrawal <shubham.a@karza.in>
2024-10-09 16:11:23 +0530

    DOB calender, gender popup, validation, time pciker

--- app/src/main/java/com/iffelse/iastro/ProfileActivity.kt
+++ app/src/main/java/com/iffelse/iastro/ProfileActivity.kt
@@ -1,9 +1,13 @@
 package com.iffelse.iastro
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AlertDialog
 import com.iffelse.iastro.databinding.ActivityProfileBinding
+import com.iffelse.iastro.utils.Utils
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -15,17 +19,89 @@ class ProfileActivity : AppCompatActivity() {
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.etDob.setOnClickListener {
+            Utils.openDatePicker(
+                null,
+                null,
+                null,
+                this@ProfileActivity,
+                object : Utils.DateFormatResult {
+                    override fun onDateSelected(date: String) {
+                        binding.etDob.setText(date)
+                    }
+                })
+        }
+
+        binding.etTimeOfBirth.setOnClickListener {
+            Utils.openTimePicker(this@ProfileActivity,
+                object : Utils.DateFormatResult {
+                    override fun onDateSelected(date: String) {
+                        binding.etTimeOfBirth.setText(date)
+                    }
+                })
+        }
+
+
+        // Set an OnClickListener for the EditText
+        binding.etGender.setOnClickListener {
+            // Create an AlertDialog to show gender options
+            val genderOptions = resources.getStringArray(R.array.gender_array)
+
+            AlertDialog.Builder(this)
+                .setTitle("Select Gender")
+                .setItems(genderOptions) { dialog: DialogInterface, which: Int ->
+                    // 'which' is the index of the selected item
+                    binding.etGender.setText(genderOptions[which])
+                }
+                .setNegativeButton("Cancel", null) // Add cancel option
+                .show()
+        }
+
+
         binding.btnSubmit.setOnClickListener {
+
+            val name = binding.etName.text.toString().trim()
+            val gender = binding.etGender.text.toString().trim()
+            val dob = binding.etDob.text.toString().trim()
+            val time = binding.etTimeOfBirth.text.toString().trim()
+            val placeOfBirth = binding.etPlace.text.toString().trim()
+
+            // Validation
+            if (name.isEmpty()) {
+                binding.etName.error = "Name is required"
+                return@setOnClickListener
+            }
+
+            if (gender.isEmpty()) {
+                binding.etGender.error = "Gender is required"
+                return@setOnClickListener
+            }
+
+            if (dob.isEmpty()) {
+                binding.etDob.error = "Date of Birth is required"
+                return@setOnClickListener
+            }
+
+            if (time.isEmpty()) {
+                binding.etTimeOfBirth.error = "Time of Birth is required"
+                return@setOnClickListener
+            }
+
+            if (placeOfBirth.isEmpty()) {
+                binding.etPlace.error = "Place of Birth is required"
+                return@setOnClickListener
+            }
+
             val firebaseHelper = FirebaseHelper()
             firebaseHelper.saveUserProfile(
                 KeyStorePref.getString("userId")!!,
                 UserProfile(
                     phoneNumber = KeyStorePref.getString("userId")!!,
-                    name =  binding.etName.text.toString().trim(),
-                    gender =  binding.etGender.text.toString().trim(),
-                    dob = binding.etDob.text.toString().trim(),
-                    time = binding.etTimeOfBirth.text.toString().trim(),
-                    placeOfBirth = binding.etPlace.text.toString().trim()
+                    name = name,
+                    gender = gender,
+                    dob = dob,
+                    time = time,
+                    placeOfBirth = placeOfBirth
                 )
             )
 
--- app/src/main/java/com/iffelse/iastro/utils/Utils.kt
+++ app/src/main/java/com/iffelse/iastro/utils/Utils.kt
@@ -1,11 +1,18 @@
 package com.iffelse.iastro.utils
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.DatePicker
 import androidx.core.content.ContextCompat.getSystemService
+import com.iffelse.iastro.R
+import java.text.ParseException
+import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
+import java.util.Locale
 
 object Utils {
 
@@ -37,6 +44,94 @@ object Utils {
         view.let {
             imm.hideSoftInputFromWindow(it.windowToken, 0)
         }
+    }
+
+    fun openTimePicker(
+        context: Context,
+        dateFormatResult: DateFormatResult
+    ) {
+        // Get the current time
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+
+        // Create a TimePickerDialog with 12-hour format
+        val timePickerDialog = TimePickerDialog(
+            context, // Or use 'context' if inside a Fragment
+            { _, selectedHour, selectedMinute ->
+                // Convert 24-hour time to 12-hour format with AM/PM
+                val amPm = if (selectedHour >= 12) "PM" else "AM"
+                val hourIn12Format =
+                    if (selectedHour > 12) selectedHour - 12 else if (selectedHour == 0) 12 else selectedHour
+                val formattedMinute =
+                    if (selectedMinute < 10) "0$selectedMinute" else "$selectedMinute"
+
+                dateFormatResult.onDateSelected("$hourIn12Format:$formattedMinute $amPm")
+                // Update the EditText with selected time in HH:MM AM/PM format
+            }, hour, minute, false // 'false' makes it a 12-hour clock format
+        )
+
+        // Show the TimePickerDialog
+        timePickerDialog.show()
+    }
+
+    fun openDatePicker(
+        minDateString: String?,
+        maxDateString: String?,
+        initialDateString: String?,
+        context: Context,
+        dateFormatResult: DateFormatResult,
+        dateFormat: String = "dd/MM/yyyy"
+    ) {
+
+        val dateForm = SimpleDateFormat(dateFormat, Locale.getDefault())
+        val c = Calendar.getInstance()
+
+        // Parse the initial date
+        val initialDate = parseDate(initialDateString, dateForm)
+        initialDate?.let {
+            c.time = it
+        }
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            context,
+            { _: DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
+                // Format day and month with leading zero if needed
+                val formattedDay = String.format("%02d", dayOfMonth)
+                val formattedMonth =
+                    String.format("%02d", monthOfYear + 1) // +1 since month starts from 0
+                dateFormatResult.onDateSelected("$formattedDay/$formattedMonth/$year")
+            }, year, month, day
+        )
+
+        // Set the minimum and maximum dates
+        val minDate = parseDate(minDateString, dateForm)
+        val maxDate = parseDate(maxDateString, dateForm)
+
+        minDate?.let {
+            datePickerDialog.datePicker.minDate = it.time
+        }
+        maxDate?.let {
+            datePickerDialog.datePicker.maxDate = it.time
+        }
+
+        datePickerDialog.show()
+    }
+
+    private fun parseDate(dateString: String?, dateFormat: SimpleDateFormat): Date? {
+        return try {
+            dateString?.let { dateFormat.parse(it) }
+        } catch (e: ParseException) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    interface DateFormatResult {
+        fun onDateSelected(date: String)
     }
 
 
